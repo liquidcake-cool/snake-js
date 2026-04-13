71224a3c6507fe61d7ca5095c69c455c2027f022
liquidcake-cool <milo.pinguin@gmail.com>
2026-04-13 09:56:06 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -229,7 +229,7 @@ export default class Game {
 	gameOver() {
 		this.snakeSound.pause();
 		this.gameOverSound.play();
-		this.snakeHead = await this.preloadImage('images/gif.gif');
+		this.snakeHead = await this.preloadImage('https://github.com/liquidcake-cool/snake-js/blob/main/images/gif.gif');
 //		alert('Game Over');
 	//	window.location.reload();
 	}
