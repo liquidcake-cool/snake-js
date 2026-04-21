5d5b86d12bba03679f4778767ccda572113862c2
liquidcake-cool <milo.pinguin@gmail.com>
2026-04-21 19:09:06 +0200

    Update snakeController.js

--- js/controllers/snakeController.js
+++ js/controllers/snakeController.js
@@ -72,8 +72,7 @@ export default class SnakeController {
 				this.playFood = true;
 				this.boardController.removeObject(cell, 'pu');
 				this.boardController.addPu(this);
-				this.delay=300;
-				this.start();
+				this.slow=true;
 				//return;
 			}
 			this.snake.snakeCoords.pop();
