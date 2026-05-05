a45aeee264fac57f5678da153d2c68cfb21625b3
liquidcake-cool <milo.pinguin@gmail.com>
2026-05-05 11:01:50 +0200

    Update snakeController.js

--- js/controllers/snakeController.js
+++ js/controllers/snakeController.js
@@ -1,4 +1,66 @@
-(
+import Game from '../game.js';
+import Snake from '../models/snake.js';
+
+export default class SnakeController {
+	constructor(context, boardController, snakeBody, snakeHead) {
+		this.init(boardController);
+		this.render(context, boardController, snakeBody, snakeHead);
+	}
+	init(boardController) {
+		this.deltaX = 0;
+		this.deltaY = -1;
+		this.snake = new Snake();
+		this.degree = 180;
+
+		for (let coord of this.snake.snakeStartCoords) {
+			let cell = boardController.getCell(coord.x, coord.y);
+			this.snake.snakeCoords.push(cell);
+		}
+	}
+	render(context, boardController, snakeBody, snakeHead) {
+		this.boardController = boardController;
+		const halfHeadSize = snakeHead.width / 2;
+		this.snake.snakeCoords.forEach((cell, i) => {
+			window.requestAnimationFrame(() => {
+				if (i === 0) {
+					context.save();
+					context.translate(
+						cell.x * boardController.cellWidth +
+							boardController.offsetX,
+						cell.y * boardController.cellheight +
+							boardController.offsetY,
+					);
+					context.translate(halfHeadSize, halfHeadSize);
+					context.rotate((this.degree * Math.PI) / 180);
+					context.drawImage(snakeHead, -halfHeadSize, -halfHeadSize);
+					context.restore();
+				} else {
+					context.drawImage(
+						snakeBody,
+						cell.x * boardController.cellWidth +
+							boardController.offsetX,
+						cell.y * boardController.cellheight +
+							boardController.offsetY,
+					);
+				}
+			});
+		});
+	}
+	move() {
+		if (!this.snake.isMoving) {
+			return;
+		}
+		let cell = this.getNextCell();
+		if (!cell || this.snake.snakeCoords.includes(cell)) {
+			this.gameOver = true;
+			return;
+		}
+
+		if (cell) {
+			this.snake.snakeCoords.unshift(cell);
+			if (cell.hasFood) {
+				this.playFood = true;
+				this.boardController.removeObject(cell, 'food');
 				this.boardController.addFood(this);
 				this.slow=false;
 				return;
@@ -13,12 +75,12 @@
 				} else{
 					this.gameOver = true; 
 				}
-				
+
 			}
 			if (cell.hasPu) {
 				this.playpu = true;
 				this.boardController.removeObject(cell, 'pu');
-				
+
 				console.log(this.boardController.punumber);
 				if (this.boardController.punumber==0){
 					this.slow=true;
